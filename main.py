45fd3610b3e62c2b39576bb3c4b6d5dc5628751d
SureshAravind123 <124656728+SureshAravind123@users.noreply.github.com>
2024-09-19 11:40:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-# main.py
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from groq import Groq
-import pyodbc
-import re
-
-app = FastAPI()
-        
-        
-
-@app.get("/getresponse")
-    return "hi"
-    
-   
     
